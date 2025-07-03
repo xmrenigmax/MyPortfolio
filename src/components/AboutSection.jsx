b0573bd6f2122436409adf4098a061739e2a04ca
xmrenigmax <RileyJordan21@hotmail.com>
2025-07-03 20:13:19 +0100

    Update: added link to CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                             <a href='#contact' className="cosmic-button">
                                 Contact Me 
                             </a>
-                            <a href="" className="px-6 py-2 rounded-full border-2 border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="/CV-1.pdf" className="px-6 py-2 rounded-full border-2 border-primary text-primary hover:bg-primary/10 transition-colors duration-300" target="_blank" rel="noopener noreferrer">
                                 Download CV
                             </a>
                         </div>
